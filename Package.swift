203b8b8eb1d8054bb16e6173bf1e97da083da330
Carlos-77Av <carlos.av.bit@gmail.com>
2026-04-19 10:07:50 -0600

    :wrench: Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AsyncFormKit",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v14)
+        .iOS(.v17)
     ],
     products: [
         .library(
